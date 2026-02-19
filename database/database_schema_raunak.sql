62922c66c25c994a6eb315f4823097ffb5135441
Raaunnakk555 <rustagiraunak@gmail.com>
2026-02-19 14:32:18 +0530

    Update database_schema_raunak.sql

--- database/database_schema_raunak.sql
+++ database/database_schema_raunak.sql
@@ -9,6 +9,27 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE IF NOT EXISTS restaurants(
+    restaurant_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT UNIQUE,
+    restaurant_name VARCHAR(150) NOT NULL,
+    address TEXT,
+    phone VARCHAR(15),
+     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS ngos (
+    ngo_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT UNIQUE,
+    ngo_name VARCHAR(150) NOT NULL,
+    address TEXT,
+    phone VARCHAR(15),
+    total_capacity_smu INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+    
+
 CREATE TABLE IF NOT EXISTS food_items (
     food_id INT AUTO_INCREMENT PRIMARY KEY,
     restaurant_id INT,
@@ -18,7 +39,9 @@ CREATE TABLE IF NOT EXISTS food_items (
     dry_or_wet ENUM('dry','wet') NOT NULL,
     calorific_value INT,
     smu_equivalent INT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (restaurant_id) REFERENCES restaurants(restaurant_id) ON DELETE CASCADE
+    
 );
 CREATE TABLE IF NOT EXISTS orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -27,7 +50,10 @@ CREATE TABLE IF NOT EXISTS orders (
     quantity_smu INT NOT NULL,
     otp VARCHAR(10),
     order_status ENUM('pending','collected','cancelled') DEFAULT 'pending',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (ngo_id) REFERENCES ngos(ngo_id) ON DELETE CASCADE,
+    FOREIGN KEY (food_id) REFERENCES food_items(food_id) ON DELETE CASCADE
 );
+
 SHOW TABLES;
 
